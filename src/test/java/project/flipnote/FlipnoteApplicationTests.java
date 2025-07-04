78d6ef8d442701e6d562507637c91030f174895a
SeokBeom Lee <91014308+stoneTiger0912@users.noreply.github.com>
2025-07-04 20:24:32 +0900

    Chore: (FN-34) CI 워크플로우 추가 (#3)
    * Chore: CI 워크플로우 추가
    
    * Chore: devlop 브랜치 수정
    
    * Chore: 테스트용 yaml 파일 추가
    
    * Chore: CI시 테스트용 yml로 빌드 테스트
    
    ---------
    
    Co-authored-by: dungbik <q1w2e30630@gmail.com>

--- src/test/java/project/flipnote/FlipnoteApplicationTests.java
+++ src/test/java/project/flipnote/FlipnoteApplicationTests.java
@@ -1,13 +1,20 @@
 package project.flipnote;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class FlipnoteApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(FlipnoteApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		log.info("테스트 확인");
 	}
 
 }
