46a8ad0758a0c5cfd7ce0247b0ffe257459f5cae
이석범 <seokbeom0912@gmail.com>
2025-07-06 21:35:59 +0900

    Feat: 에러 코드 추가

--- src/main/java/project/flipnote/group/exception/GroupErrorCode.java
+++ src/main/java/project/flipnote/group/exception/GroupErrorCode.java
@@ -1,4 +1,24 @@
 package project.flipnote.group.exception;
 
-public enum GroupErrorCode {
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import project.flipnote.common.exception.ErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum GroupErrorCode implements ErrorCode {
+
+	;
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+
+	@Override
+	public int getStatus() {
+		return httpStatus.value();
+	}
 }
