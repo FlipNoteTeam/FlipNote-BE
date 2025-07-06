61f1fed1318fa0a6d81d2e4874d7d7f637b908ab
dungbik <q1w2e30630@gmail.com>
2025-07-06 16:19:08 +0900

    Feat: 스레드 풀 설정에 유효성 검증(@Min) 추가

--- src/main/java/project/flipnote/common/config/AsyncProperties.java
+++ src/main/java/project/flipnote/common/config/AsyncProperties.java
@@ -3,6 +3,7 @@ package project.flipnote.common.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,13 @@ import lombok.Setter;
 @ConfigurationProperties(prefix = "async")
 @Component
 public class AsyncProperties {
+
+	@Min(value = 1)
 	private int corePoolSize;
+
+	@Min(value = 1)
 	private int maxPoolSize;
+
+	@Min(value = 1)
 	private int queueCapacity;
 }
