8a47f7c1d339a234a843c3c4504a4cfbdec3d55c
dungbik <q1w2e30630@gmail.com>
2025-07-06 16:27:11 +0900

    Feat: Resend 설정에 유효성 검증(@NotEmpty) 추가

--- src/main/java/project/flipnote/infra/config/ResendProperties.java
+++ src/main/java/project/flipnote/infra/config/ResendProperties.java
@@ -3,6 +3,7 @@ package project.flipnote.infra.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,10 @@ import lombok.Setter;
 @ConfigurationProperties("resend")
 @Component
 public class ResendProperties {
+
+	@NotEmpty
 	private String fromEmail;
+
+	@NotEmpty
 	private String apiKey;
 }
