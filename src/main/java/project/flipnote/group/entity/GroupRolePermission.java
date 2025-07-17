1f05bddb36b71baf9e31ca20d044e8009a408a23
이석범 <seokbeom0912@gmail.com>
2025-07-17 17:31:53 +0900

    Refactor: 그룹 아이디, 권한 아이디, 역할이 중복 생성되는 것을 방지하기 위해 유니크 추가

--- src/main/java/project/flipnote/group/entity/GroupRolePermission.java
+++ src/main/java/project/flipnote/group/entity/GroupRolePermission.java
@@ -1,15 +1,6 @@
 package project.flipnote.group.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,7 +9,10 @@ import lombok.NoArgsConstructor;
 각 그룹 내에서 역할별로 가지는 권한들을 관리하는 엔티티
  */
 @Entity
-@Table(name = "group_role_permissions")
+@Table(name = "group_role_permissions",
+		uniqueConstraints = @UniqueConstraint(
+				columnNames = {"group_id", "group_permission_id", "role"}
+		))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GroupRolePermission {
