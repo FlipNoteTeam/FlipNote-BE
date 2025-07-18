2c5e09645d7b24ac072c1d67096d85d2c133bc2c
stoneTiger0912 <seokbeom0912@gmail.com>
2025-07-18 11:20:34 +0900

    Refactor: repository 어노테이션 추가

--- src/main/java/project/flipnote/group/repository/GroupRolePermissionRepository.java
+++ src/main/java/project/flipnote/group/repository/GroupRolePermissionRepository.java
@@ -2,7 +2,9 @@ package project.flipnote.group.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import project.flipnote.group.entity.GroupRolePermission;
 
+@Repository
 public interface GroupRolePermissionRepository extends JpaRepository<GroupRolePermission, Long> {
 }
