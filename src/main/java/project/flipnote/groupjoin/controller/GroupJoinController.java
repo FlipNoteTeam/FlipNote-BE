d741e7bd0fc4410d04577851f83ade23c4cde559
stoneTiger0912 <seokbeom0912@gmail.com>
2025-08-05 23:39:33 +0900

    Merge: 머지 충돌 해결

--- src/main/java/project/flipnote/groupjoin/controller/GroupJoinController.java
+++ src/main/java/project/flipnote/groupjoin/controller/GroupJoinController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
-import project.flipnote.common.security.dto.UserPrincipal;
+import project.flipnote.common.security.dto.AuthPrinciple;
 import project.flipnote.groupjoin.model.*;
 import project.flipnote.groupjoin.service.GroupJoinService;
 
@@ -20,10 +20,10 @@ public class GroupJoinController {
 	//가입 신청 요청
 	@PostMapping("/groups/{groupId}/joins")
 	public ResponseEntity<GroupJoinResponse> joinRequest(
-			UserPrincipal userPrincipal,
+			AuthPrinciple authPrinciple,
 			@PathVariable("groupId") Long groupId,
 			@Valid @RequestBody GroupJoinRequest req) {
-		GroupJoinResponse res = groupJoinService.joinRequest(userPrincipal, groupId, req);
+		GroupJoinResponse res = groupJoinService.joinRequest(authPrinciple, groupId, req);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(res);
 	}
@@ -31,9 +31,9 @@ public class GroupJoinController {
 	//그룹 내 가입 신청한 리스트 조회
 	@GetMapping("/groups/{groupId}/joins")
 	public ResponseEntity<GroupJoinListResponse> findGroupJoinList(
-			UserPrincipal userPrincipal,
+			AuthPrinciple authPrinciple,
 			@PathVariable("groupId") Long groupId) {
-		GroupJoinListResponse res = groupJoinService.findGroupJoinList(userPrincipal, groupId);
+		GroupJoinListResponse res = groupJoinService.findGroupJoinList(authPrinciple, groupId);
 
 		return ResponseEntity.ok(res);
 	}
@@ -41,12 +41,12 @@ public class GroupJoinController {
 	//가입 신청 응답
 	@PatchMapping("/groups/{groupId}/joins/{joinId}")
 	public ResponseEntity<GroupJoinRespondResponse> respondToJoinRequest(
-			UserPrincipal userPrincipal,
+			AuthPrinciple authPrinciple,
 			@PathVariable("groupId") Long groupId,
 			@PathVariable("joinId") Long joinId,
 			@Valid @RequestBody GroupJoinRespondRequest req) {
 
-		GroupJoinRespondResponse res = groupJoinService.respondToJoinRequest(userPrincipal, groupId, joinId, req);
+		GroupJoinRespondResponse res = groupJoinService.respondToJoinRequest(authPrinciple, groupId, joinId, req);
 
 		return ResponseEntity.ok(res);
 	}
@@ -54,11 +54,11 @@ public class GroupJoinController {
 	//가입 신청 삭제
 	@DeleteMapping("/groups/{groupId}/joins/{joinId}")
 	public ResponseEntity<Void> groupJoinDelete(
-		UserPrincipal userPrincipal,
+		AuthPrinciple authPrinciple,
 		@PathVariable("groupId") Long groupId,
 		@PathVariable("joinId") Long joinId
 	) {
-		groupJoinService.groupJoinDelete(userPrincipal, groupId, joinId);
+		groupJoinService.groupJoinDelete(authPrinciple, groupId, joinId);
 
 		return ResponseEntity.noContent().build();
 	}
@@ -66,9 +66,9 @@ public class GroupJoinController {
 	//내가 신청한 가입신청 리스트 조회
 	@GetMapping("/groups/joins/me")
 	public ResponseEntity<FIndGroupJoinListMeResponse> findGroupJoinMe(
-		UserPrincipal userPrincipal
+		AuthPrinciple authPrinciple
 	) {
-		FIndGroupJoinListMeResponse res = groupJoinService.findGroupJoinListMe(userPrincipal);
+		FIndGroupJoinListMeResponse res = groupJoinService.findGroupJoinListMe(authPrinciple);
 
 		return ResponseEntity.ok(res);
 	}
