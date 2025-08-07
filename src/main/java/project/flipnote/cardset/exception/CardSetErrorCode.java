79a674ca93c0768216fd0ff44ee97315cdb6bfa8
stoneTiger0912 <seokbeom0912@gmail.com>
2025-08-07 23:33:43 +0900

    Fix: 카드셋 생성시 에러 클래스 변경

--- src/main/java/project/flipnote/cardset/exception/CardSetErrorCode.java
+++ src/main/java/project/flipnote/cardset/exception/CardSetErrorCode.java
@@ -10,7 +10,7 @@ import project.flipnote.common.exception.ErrorCode;
 @RequiredArgsConstructor
 public enum CardSetErrorCode implements ErrorCode {
 
-	;
+	GROUP_MEMBER_NOT_FOUND(HttpStatus.FORBIDDEN, "CARDSET_001", "해당 그룹의 멤버가 아닙니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
