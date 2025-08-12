4c632a5d7c9aaaee6348f93694b95cf12013e2c9
dungbik <q1w2e30630@gmail.com>
2025-08-12 17:58:55 +0900

    Docs: 그룹 초대 API Swagger Docs에 보안 스키마 명시

--- src/main/java/project/flipnote/group/controller/docs/GroupInvitationControllerDocs.java
+++ src/main/java/project/flipnote/group/controller/docs/GroupInvitationControllerDocs.java
@@ -3,6 +3,7 @@ package project.flipnote.group.controller.docs;
 import org.springframework.http.ResponseEntity;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import project.flipnote.common.security.dto.AuthPrinciple;
 import project.flipnote.group.model.GroupInvitationCreateRequest;
@@ -12,15 +13,15 @@ import project.flipnote.group.model.GroupInvitationRespondRequest;
 @Tag(name = "Group Invitation", description = "Group Invitation API")
 public interface GroupInvitationControllerDocs {
 
-	@Operation(summary = "그룹 초대")
+	@Operation(summary = "그룹 초대", security = {@SecurityRequirement(name = "access-token")})
 	ResponseEntity<GroupInvitationCreateResponse> createGroupInvitation(
 		Long groupId, GroupInvitationCreateRequest req, AuthPrinciple authPrinciple
 	);
 
-	@Operation(summary = "그룹 초대 취소")
+	@Operation(summary = "그룹 초대 취소", security = {@SecurityRequirement(name = "access-token")})
 	ResponseEntity<Void> deleteGroupInvitation(Long groupId, Long invitationId, AuthPrinciple authPrinciple);
 
-	@Operation(summary = "그룹 초대 응답")
+	@Operation(summary = "그룹 초대 응답", security = {@SecurityRequirement(name = "access-token")})
 	ResponseEntity<Void> respondToGroupInvitation(
 		Long groupId, Long invitationId, GroupInvitationRespondRequest req, AuthPrinciple authPrinciple
 	);
--- src/main/java/project/flipnote/group/controller/docs/GroupInvitationQueryControllerDocs.java
+++ src/main/java/project/flipnote/group/controller/docs/GroupInvitationQueryControllerDocs.java
@@ -3,16 +3,17 @@ package project.flipnote.group.controller.docs;
 import org.springframework.http.ResponseEntity;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import project.flipnote.common.response.PageResponse;
 import project.flipnote.common.security.dto.AuthPrinciple;
-import project.flipnote.group.model.OutgoingGroupInvitationResponse;
 import project.flipnote.group.model.IncomingGroupInvitationResponse;
+import project.flipnote.group.model.OutgoingGroupInvitationResponse;
 
 @Tag(name = "Group Invitation Query", description = "Group Invitation Query API")
 public interface GroupInvitationQueryControllerDocs {
 
-	@Operation(summary = "그룹 초대 보낸 목록 조회")
+	@Operation(summary = "그룹 초대 보낸 목록 조회", security = {@SecurityRequirement(name = "access-token")})
 	ResponseEntity<PageResponse<OutgoingGroupInvitationResponse>> getOutgoingInvitations(
 		Long groupId,
 		int page,
@@ -20,7 +21,7 @@ public interface GroupInvitationQueryControllerDocs {
 		AuthPrinciple authPrinciple
 	);
 
-	@Operation(summary = "그룹 초대 받은 목록 조회")
+	@Operation(summary = "그룹 초대 받은 목록 조회", security = {@SecurityRequirement(name = "access-token")})
 	ResponseEntity<PageResponse<IncomingGroupInvitationResponse>> getIncomingInvitations(
 		int page,
 		int size,
