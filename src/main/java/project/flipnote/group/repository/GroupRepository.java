bd3b8f808b3638588206b8e8d72b9758a4b79665
dungbik <q1w2e30630@gmail.com>
2025-08-13 18:00:48 +0900

    Chore: import 실수 수정

--- src/main/java/project/flipnote/group/repository/GroupRepository.java
+++ src/main/java/project/flipnote/group/repository/GroupRepository.java
@@ -5,16 +5,16 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import project.flipnote.group.entity.Group;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
-  Optional<Group> findByIdAndDeletedAtIsNull(Long groupId);
+	Optional<Group> findByIdAndDeletedAtIsNull(Long groupId);
 
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("select g from Group g where g.id = :id")
