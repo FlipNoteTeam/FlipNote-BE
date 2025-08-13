aae6bf467c12022a2d7b546bcbe7a587ce64cdad
stoneTiger0912 <seokbeom0912@gmail.com>
2025-08-13 23:50:31 +0900

    Feat: 그룹 삭제 API 추가

--- src/main/java/project/flipnote/group/controller/GroupController.java
+++ src/main/java/project/flipnote/group/controller/GroupController.java
@@ -3,6 +3,7 @@ package project.flipnote.group.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import project.flipnote.group.service.GroupService;
 public class GroupController {
 	private final GroupService groupService;
 
+	//그룹 생성 API
 	@PostMapping("")
 	public ResponseEntity<GroupCreateResponse> create(
 		@AuthenticationPrincipal AuthPrinciple authPrinciple,
@@ -32,6 +34,7 @@ public class GroupController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(res);
 	}
 
+	//그룹 상세 API
 	@GetMapping("/{groupId}")
 	public ResponseEntity<GroupDetailResponse> findGroupDetail(
 		@AuthenticationPrincipal AuthPrinciple authPrinciple,
@@ -39,5 +42,17 @@ public class GroupController {
 		GroupDetailResponse res = groupService.findGroupDetail(authPrinciple, groupId);
 
 		return ResponseEntity.ok(res);
+	}
+
+	//그룹 삭제 API
+	@DeleteMapping("/{groupId}")
+	public ResponseEntity<Void> deleteGroup(
+		@AuthenticationPrincipal AuthPrinciple authPrinciple,
+		@PathVariable("groupId") Long groupId
+	) {
+
+		groupService.deleteGroup(authPrinciple, groupId);
+
+		return ResponseEntity.noContent().build();
 	}
 }
