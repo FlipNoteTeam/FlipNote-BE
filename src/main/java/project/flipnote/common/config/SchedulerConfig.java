4e4aff38e81546da1af7a93d4cfde3960d644e6f
이석범 <seokbeom0912@gmail.com>
2025-09-11 23:28:31 +0900

    Feat: 스케쥴링 시간 변경

--- src/main/java/project/flipnote/common/config/SchedulerConfig.java
+++ src/main/java/project/flipnote/common/config/SchedulerConfig.java
@@ -18,15 +18,15 @@ public class SchedulerConfig {
 	private final ImageCleanService imageCleanService;
 
 	//이미지 참조 제거
-	// @Scheduled(cron = "0 0 23 * * *", zone = "Asia/Seoul")
-	@Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
+	@Scheduled(cron = "0 0 23 * * *", zone = "Asia/Seoul")
+	// @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
 	public void cleanImageRef() {
 		imageCleanService.cleanImageRef();
 	}
 	
 	//이미지 제거
-	// @Scheduled(cron = "0 30 23 * * 0", zone = "Asia/Seoul")
-	@Scheduled(cron = "30 */2 * * * *", zone = "Asia/Seoul")
+	@Scheduled(cron = "0 30 23 * * 0", zone = "Asia/Seoul")
+	// @Scheduled(cron = "30 */2 * * * *", zone = "Asia/Seoul")
 	public void cleanImage() {
 		imageCleanService.cleanImage();
 	}
