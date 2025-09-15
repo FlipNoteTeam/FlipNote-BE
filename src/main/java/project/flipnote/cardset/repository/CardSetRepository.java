e52bf503c54dce46cdc3a15ed9a65516c2e4f0ce
dungbik <q1w2e30630@gmail.com>
2025-09-16 00:07:22 +0900

    Chore: import 빠진 부분 추가

--- src/main/java/project/flipnote/cardset/repository/CardSetRepository.java
+++ src/main/java/project/flipnote/cardset/repository/CardSetRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import project.flipnote.cardset.entity.CardSet;
