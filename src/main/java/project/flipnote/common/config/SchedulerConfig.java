fc6a7a9ad4ac88bc444da8d92c28c7462105ab0e
이석범 <seokbeom0912@gmail.com>
2025-09-15 20:56:16 +0900

    Fix: 락 설정 수정

--- src/main/java/project/flipnote/common/config/SchedulerConfig.java
+++ src/main/java/project/flipnote/common/config/SchedulerConfig.java
@@ -5,12 +5,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 import lombok.RequiredArgsConstructor;
 import project.flipnote.image.service.ImageCleanService;
 
 @RequiredArgsConstructor
-@EnableSchedulerLock(defaultLockAtMostFor = "2m")
 @EnableScheduling
 @Configuration
 public class SchedulerConfig {
@@ -20,12 +20,14 @@ public class SchedulerConfig {
 	//이미지 참조 제거
 	@Scheduled(cron = "0 0 23 * * *", zone = "Asia/Seoul")
 	// @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
+	@SchedulerLock(name = "image.cleanImageRef", lockAtMostFor = "PT2M")
 	public void cleanImageRef() {
 		imageCleanService.cleanImageRef();
 	}
 	
 	//이미지 제거
 	@Scheduled(cron = "0 30 23 * * 0", zone = "Asia/Seoul")
+	@SchedulerLock(name = "image.cleanImage", lockAtMostFor = "PT2M")
 	// @Scheduled(cron = "30 */2 * * * *", zone = "Asia/Seoul")
 	public void cleanImage() {
 		imageCleanService.cleanImage();
