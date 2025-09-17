2e8c3212961443bea528d9679d4d00bdf83f6e63
dungbik <q1w2e30630@gmail.com>
2025-09-17 17:15:09 +0900

    Feat: 카드셋 목록 조회시 즐겨찾기 수 정렬 기능

--- src/main/java/project/flipnote/cardset/model/CardSetSortField.java
+++ src/main/java/project/flipnote/cardset/model/CardSetSortField.java
@@ -5,7 +5,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum CardSetSortField {
-	ID, LIKE;
+	ID, LIKE, BOOKMARK;
 
 	public static Set<String> getFieldNames() {
 		return Arrays.stream(values())
